c5a3eccefa4d3d3790eee5067d028994efddb642
ThuOanh229 <thuoanh229@gmail.com>
2018-03-22 08:32:44 +0700

    Up_GitHub

---
+++ smart_shop/smart_shop/smart_shop/Models/ItemGioHang.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace smart_shop.Models
+{
+    public class ItemGioHang
+    {
+        public int MaSP { get; set; }
+        public string TenSP { get; set; }
+        public int SoLuong { get; set; }
+        public string HinhAnh { get; set; }
+        public decimal? DonGia { get; set; }
+        public decimal? ThanhTien { get; set; }
+
+        public ItemGioHang(int maSP)
+        {
+            using (smart_shopEntities db = new smart_shopEntities())
+            {
+                sanpham sp = db.sanphams.Single(n => n.MaSanPham == maSP);
+                MaSP = maSP;
+                TenSP = sp.TenSanPham;
+                SoLuong = 1;
+                HinhAnh = sp.HinhAnh1;
+                DonGia = sp.DonGia;
+                //ThanhTien = SoLuong * DonGia;
+            }
+        }
+        public ItemGioHang(int maSP,int sl)
+        {
+            using (smart_shopEntities db = new smart_shopEntities())
+            {
+                sanpham sp = db.sanphams.Single(n => n.MaSanPham == maSP);
+                MaSP = maSP;
+                TenSP = sp.TenSanPham;
+                SoLuong = sl;
+                HinhAnh = sp.HinhAnh1;
+                DonGia = sp.DonGia;
+                //ThanhTien = SoLuong * DonGia;
+            }
+        }
+        public ItemGioHang()
+        {
+
+        }
+    }
+}
